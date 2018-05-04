7582dd2c8709e177a7d52e7e9f612ff75f8217e3
diandra <diandramutiar@gmail.com>
2018-05-04 21:13:39 +0700

    first init

---
+++ app/src/main/java/com/diandra/spensamtl/MainActivity.java
@@ -0,0 +1,54 @@
+package com.diandra.spensamtl;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.navigation_beranda:
+                    fragment = new BerandaFragment();
+                    break;
+                case R.id.navigation_pengumuman:
+                    fragment = new PengumumanFragment();
+                    break;
+                case R.id.navigation_lainnya:
+                    fragment = new LainnyaFragment();
+                    break;
+            }
+
+            if (fragment!=null) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
