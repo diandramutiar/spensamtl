7dde6d4b408e7a68bdbee8e3c8e322d7c002e65c
diandra <diandramutiar@gmail.com>
2018-05-15 23:09:45 +0700

    lainnya

--- app/src/main/java/com/diandra/spensamtl/KontakFragment.java
+++ app/src/main/java/com/diandra/spensamtl/KontakFragment.java
@@ -23,6 +23,7 @@ public class KontakFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().setTitle("Kontak");
         return inflater.inflate(R.layout.fragment_kontak, container, false);
     }
 
